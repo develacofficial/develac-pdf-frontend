739758ede2583f47e7f8c63a23ad5c4218b321e2
develacofficial <develacofficial@gmail.com>
2026-02-03 22:55:05 +0530

    Responsive menu

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,1 +1,6 @@
+const menuBtn = document.getElementById("menuBtn");
+const mobileNav = document.getElementById("mobileNav");
 
+menuBtn.addEventListener("click", () => {
+  mobileNav.classList.toggle("open");
+});
